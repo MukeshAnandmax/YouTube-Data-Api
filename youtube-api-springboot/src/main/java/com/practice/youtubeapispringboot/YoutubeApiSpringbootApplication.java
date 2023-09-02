f33ac7bc0a8fa05869f969b4fe837b5d9c490fee
Mukesh Anand <Mukeshanandmax@gmail.com>
2023-09-02 12:11:33 +0530

    initial Commit

---
+++ youtube-api-springboot/src/main/java/com/practice/youtubeapispringboot/YoutubeApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.practice.youtubeapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeApiSpringbootApplication.class, args);
+	}
+
+}
---
+++ youtube-api-springboot/src/test/java/com/practice/youtubeapispringboot/YoutubeApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.youtubeapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
